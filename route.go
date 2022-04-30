a0a18a0c6ee8884390f76cb0f08a0f75dc6b01d3
Amit <amit.silicon2008@gmail.com>
2022-04-30 16:17:23 +0530

    Route api call

--- route.go
+++ route.go
@@ -14,25 +14,14 @@ func main() {
 	const (
 		namespace = "test"
 		routeName = "nodejs-basic"
-		// consoleLinkName = "argocd"
 	)
 
 	var k8sClient client.Client
 
 	route := &routev1.Route{}
-	// consoleLink := &console.ConsoleLink{}
-
-	// err := k8sClient.Get(context.TODO(), types.NamespacedName{Name: consoleLinkName}, consoleLink)
-	// if err != nil {
-	// 	fmt.Println("link not found")
-	// }
 	err := k8sClient.Get(context.TODO(), types.NamespacedName{Name: routeName, Namespace: namespace}, route)
 	if err != nil {
-		fmt.Println("Route not found")
+		fmt.Println("Some issue")
 	}
-	// clLink := strings.TrimLeft(consoleLink.Spec.Href, "https://")
-	// routeLink := route.Spec.Host
-	// if clLink != routeLink {
-	// 	fmt.Println("URL mismatch, route: %s, consoleLink: %s", routeLink, clLink)
-	// }
+	fmt.Println("Api call completed")
 }
